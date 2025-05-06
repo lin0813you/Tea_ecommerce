8f2a493d8f39fd57b2e8db6c1c54b7b91d15bda4
lin0813you <lin32247070@gmail.com>
2025-05-06 15:02:12 +0800

    adjust NavBar (fixed)

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+// src/components/NavBar.jsx
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import './NavBar.scss'
+
+export default function NavBar({ user, cartCount }) {
+  return (
+    <Navbar fixed="top" bg="light" className="navbar-teashop">
+      <Container>
+        {/* 品牌／標題 */}
+        <Navbar.Brand href="/" className="navbar-teashop__brand">
+          高師獅手搖茶飲
+        </Navbar.Brand>
+
+        {/* 永遠展開的水平選單 */}
+        <Nav className="navbar-teashop__nav ms-auto">
+          <Nav.Link href="/">首頁</Nav.Link>
+          <Nav.Link href="/products">所有商品</Nav.Link>
+          <Nav.Link href="/cart" className="nav-icon-link">
+            <FaShoppingCart />
+            購物車
+            {cartCount > 0 && (
+              <span className="cart-badge">{cartCount}</span>
+            )}
+          </Nav.Link>
+
+          {user ? (
+            <Nav.Link href="/profile" className="nav-icon-link">
+              <FaUser />
+              {user.name}
+            </Nav.Link>
+          ) : (
+            <>
+              <Nav.Link href="/login">登入</Nav.Link>
+              <Nav.Link href="/register">註冊</Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
--- client/src/pages/CustomerMainPage.jsx
+++ client/src/pages/CustomerMainPage.jsx
@@ -1,39 +1,51 @@
-import { useEffect, useState } from 'react';
-import { fetchProducts } from '../api/product';
-import ProductCard from '../components/ProductCard';
-import SearchBar from '../components/SearchBar';
+// src/pages/CustomerMainPage.jsx
+import { useEffect, useState } from 'react'
+import { fetchProducts }    from '../api/product'
+import ProductCard          from '../components/ProductCard'
+import SearchBar            from '../components/SearchBar'
+import NavBar               from '../components/NavBar'
+import './CustomerMainPage.scss'
 
 export default function CustomerMainPage() {
-  const [products, setProducts] = useState([]); 
-  const [query, setQuery] = useState('');
+  const [products, setProducts] = useState([])
+  const [query,    setQuery]    = useState('')
 
- 
+  // TODO: 之後從 Context 或 API 拿到真正的 user/cartCount
+  const mockUser      = { name: '小明' }
+  const mockCartCount = 3
+
   useEffect(() => {
     fetchProducts()
       .then(data => {
-        console.log('API 回傳：', data);
-        // 假設 data 是陣列，就直接設
-        setProducts(Array.isArray(data) ? data : data.products);
+        setProducts(Array.isArray(data) ? data : data.products)
       })
-      .catch(err => console.error(err));
-  }, []);
-  
+      .catch(err => console.error(err))
+  }, [])
 
   const filtered = (products ?? []).filter(p =>
     p.name.toLowerCase().includes(query.toLowerCase())
-  );
+  )
 
   return (
-    <div className="container py-4">
-      <h1 className="mb-4">茶飲商城</h1>
-      <SearchBar value={query} onChange={setQuery} />
-      <div className="row g-4">
-        {filtered.map(p => (
-          <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <ProductCard product={p} />
-          </div>
-        ))}
+    <>
+      {/* 1. 導覽列放最上方 */}
+      <NavBar user={mockUser} cartCount={mockCartCount} />
+
+      {/* 2. 主內容容器 */}
+      <div className="container py-4 main-container">
+
+        {/* 3. 搜尋列 */}
+        <SearchBar value={query} onChange={setQuery} />
+
+        {/* 4. 商品列表 */}
+        <div className="row g-4">
+          {filtered.map(p => (
+            <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <ProductCard product={p} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    </>
+  )
 }
