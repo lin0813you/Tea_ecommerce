132d1d56fb61199545b28834b907940060a4a704
lin0813you <lin32247070@gmail.com>
2025-05-07 02:30:18 +0800

    adjust prodectCard component

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,8 @@
-// src/components/ProductCard.jsx
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'  // 暫時註解掉
 
-
-export default function ProductCard({ product, onAddCart }) {
+export default function ProductCard({ product /*, onAddCart */ }) {
   return (
     <Card className="product-card">
       <div className="product-card__image-wrapper">
@@ -19,9 +17,13 @@ export default function ProductCard({ product, onAddCart }) {
         <Card.Title className="product-card__title">
           {product.name}
         </Card.Title>
+
+        {/* 新增 description 區塊 */}
         <Card.Text className="product-card__description">
           {product.description}
         </Card.Text>
+
+        {/*
         <div className="product-card__footer">
           <span className="product-card__price">
             ${product.price.toFixed(2)}
@@ -33,6 +35,7 @@ export default function ProductCard({ product, onAddCart }) {
             加入購物車
           </Button>
         </div>
+        */}
       </Card.Body>
     </Card>
   )
