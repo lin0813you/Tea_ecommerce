f9b9dacff10376f0eca5d7dbd236d315d0596e2d
lin32247070 <lin32247070@gmail.com>
2025-05-12 00:45:17 +0000

    delete chakra packgae

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CustomerMainPage from './pages/CustomerMainPage';
 import ProductSearchPage from './pages/ProductSearchPage';
+import NavBar from './components/NavBar';
 
 export default function App() {
+  // TODO: 之後從 Context 或 API 拿到真正的 user/cartCount
+  const mockUser = { name: "小明" };
+  const mockCartCount = 3;
+
   return (
     <>
       <NavBar user={mockUser} cartCount={mockCartCount} />
@@ -14,6 +19,3 @@ export default function App() {
     </>
   )
 }
-
-export default App
-
