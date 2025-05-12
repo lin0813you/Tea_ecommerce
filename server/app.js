54bc26942c57d1ed2acfd66f8cd09f4ce91feb67
lin32247070 <lin32247070@gmail.com>
2025-05-12 20:31:53 +0000

    save (05/13 04:31)

--- server/app.js
+++ server/app.js
@@ -2,16 +2,31 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import productsRouter from "./routes/productsRouter";
+import sequelize from './config/database';
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/products", productsRouter);
+
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+    app.listen(port, () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+startServer();
