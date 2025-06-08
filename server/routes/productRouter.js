c795867eda27d74282618b6af45ae2c4377a4542
seeYou <120365880+lin0813you@users.noreply.github.com>
2025-06-09 01:43:43 +0800

    chore: rename folders to plural and update imports

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} from '../controllers/ProductController.js';
 
 const router = express.Router();
 
-
-router.get('/products', () => {});
+router.get('/products', getProducts);
+router.get('/products/:id', getProduct);
+router.post('/products', createProduct);
+router.put('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
 
 export default router;
