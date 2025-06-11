54d6e8a3db6c6ca545c36856cec7200f3d882056
lin0813you <lin32247070@gmail.com>
2025-06-11 23:34:43 +0800

    fix bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,7 +19,6 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:3000", // Assuming your backend is on port 3000
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
